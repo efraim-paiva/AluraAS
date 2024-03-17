5805420522743464685324d6055ec71d36eb413a
efraim-paiva <efgaroto@gmail.com>
2024-03-17 10:46:08 -0300

    Add goals

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+let nomeAmigo = document.getElementById('nome-amigo');
+let listaNomes = document.getElementById('lista-amigos');
+
+function adicionar() {
+
+    
+
+    //pegar o valor do input
+    //adicinar o valor do input no array amigos incluidos
+    //limpar o input
+    //
+
+
+
+}
+
+function sortear() {
+    //selecionar um valor do input de erray amigos de forma aleatoria 
+}
+
+function reiniciar() {
+    //limpa o array de amigos incluidos
+    //limpa o input
+    //limpa o resultado
+
+
+}
